6dcd228ffe5848a250308bf6f70168506082ca84
AgustinMDominguez <agustinmarcelodominguez@gmail.com>
2023-08-06 14:32:01 -0300

    AMD

---
+++ scripts/update_w_from_parent.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#
+## @file                update_w_from_parent.sh
+## @author              Valentina Vispo
+## @section LICENSE     Apache
+
+folderToSearch='.github'
+
+echo -e 'Creando carpeta .github/\n'
+mkdir -p .github/
+echo -e 'Configurando workflows...\n'
+cp -r parentRepository/child.github/* .github/
+# mv useFolder/ .github/workflows
+# rm .github/workflows/list_auto_assign.yml
+# rm -rf .github/workflows/useFolder/
+# rm -rf .github/workflows/.github
+# cp utilsWorkflows/labeler.yml .github/
+# cp utilsWorkflows/list_auto_assign.yml .github/
+echo -e '\nConfiguracion completa.'
+exit 1
