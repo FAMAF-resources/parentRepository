97271938aa94843a0f9218f4c2aec13b984adba6
AgustinMDominguez <agustinmarcelodominguez@gmail.com>
2023-08-06 15:22:11 -0300

    clean update bash script

--- scripts/update_w_from_parent.sh
+++ scripts/update_w_from_parent.sh
@@ -4,20 +4,9 @@
 ## @author              Valentina Vispo
 ## @section LICENSE     Apache
 
-folderToSearch='.github'
-
 echo -e 'Running parent update files script'
-pwd
 echo -e 'Creating .github/ folder\n'
 mkdir -p .github/
 echo -e 'Updating workflows...\n'
-ls -Rla parentRepository/child.github
 cp -r parentRepository/child.github/* .github/
-ls -Rla .github/
-# mv useFolder/ .github/workflows
-# rm .github/workflows/list_auto_assign.yml
-# rm -rf .github/workflows/useFolder/
-# rm -rf .github/workflows/.github
-# cp utilsWorkflows/labeler.yml .github/
-# cp utilsWorkflows/list_auto_assign.yml .github/
-echo -e '\nConfiguracion completa.'
+echo -e '\nUpdate complete'
